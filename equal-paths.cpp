113d9aeb6594bb544cb4877468e0a0538ff6cfb4
Richard Tibor Fober <fober@usc.edu>
2025-03-23 01:37:46 +0000

    equal paths functions added

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,11 +8,31 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+bool checkEqualPaths(Node* node, int depth, int& leafDepth)
+{
+  if(node == nullptr) return true;
 
+  // if leaf
+  if(node->left == nullptr && node->right == nullptr)
+  {
+    if(leafDepth == -1)
+    {
+      leafDepth = depth;
+      return true;
+    }
+    return depth == leafDepth;
+  }
+
+  bool left = checkEqualPaths(node->left, depth + 1, leafDepth);
+  bool right = checkEqualPaths(node->right, depth + 1, leafDepth);
+
+  return (left && right);
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
-
+    int leafDepth = -1;
+    return checkEqualPaths(root, 0, leafDepth);
 }
 
